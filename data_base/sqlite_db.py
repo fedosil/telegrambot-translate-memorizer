23e04be0a027828f8290b288b8f11146b8f0b25f
fedosil <fedosil@yandex.ru>
2022-12-13 13:28:44 +0300

    Update sqlite_db.py

--- data_base/sqlite_db.py
+++ data_base/sqlite_db.py
@@ -1,4 +1,5 @@
 import sqlite3 as sq
+from datetime import datetime
 
 
 def sql_start():
@@ -8,51 +9,49 @@ def sql_start():
     if base:
         print('SQLite base connected OK')
 
+
 def sql_close():
     cursor.close()
     base.close()
 
+
 def sql_add_id_table_command(id):
+    current_date = datetime.now().strftime('%Y %m %d')
     base.execute(
-        f"""CREATE TABLE IF NOT EXISTS dict_en_user_id_%s
-            (
-                id_word int GENERATED ALWAYS AS IDENTITY,
-    	        en_word varchar(64) UNIQUE,
-    	        ru_word varchar(64) UNIQUE,
-                repeat_date date DEFAULT NOW()
-            );""", (id,)
+        f"""CREATE TABLE IF NOT EXISTS dict_en_user_id_{id}
+        (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
+        en_word text NOT NULL UNIQUE, 
+        ru_word text NOT NULL, 
+        repeat_date text DEFAULT CURRENT_TIMESTAMP);"""
     )
     base.commit()
 
 
 def sql_add_text_command(id, en, ru):
+    current_date = datetime.now().strftime('%Y %m %d')
     cursor.execute(
-        f"""INSERT INTO dict_en_user_id_%s(en_word, ru_word)
-                VALUES
-                (%s, %s);""", (id, en, ru)
+        f"""INSERT INTO dict_en_user_id_{id}(en_word, ru_word) VALUES (?, ?);""", (en, ru)
     )
     base.commit()
 
 
 def sql_read_base(id):
     cursor.execute(
-        f"""SELECT * FROM dict_en_user_id_%s
-                ORDER BY repeat_date;""", (id,)
+        f"""SELECT * FROM dict_en_user_id_{id} ORDER BY repeat_date;"""
     )
     return cursor.fetchall()
 
+
 def sql_update_date(id, en):
+    current_date = datetime.now().strftime('%Y %m %d')
     cursor.execute(
-        f"""UPDATE dict_en_user_id_%s
-            SET repeat_date = NOW()
-            WHERE en_word = %s""", (id, en)
+        f"""UPDATE dict_en_user_id_{id} SET repeat_date = CURRENT_TIMESTAMP WHERE en_word = ?""", (en,)
     )
     base.commit()
 
 
 def sql_del_command(id, en):
     cursor.execute(
-        f"""DELETE FROM dict_en_user_id_%s
-                WHERE en_word = %s;""", (id, en)
+        f"""DELETE FROM dict_en_user_id_{id} WHERE en_word = ?;""", (en,)
     )
     base.commit()
