8b731e27c9b59fd3d20f7d2e2af63de8f40a5ec7
fedosil <fedosil@yandex.ru>
2022-12-13 11:24:30 +0300

    change sqlite funcs

--- data_base/sql_db.py
+++ data_base/sql_db.py
@@ -3,28 +3,34 @@ from data_base import postgesql_db
 
 
 def sql_start():
-    postgesql_db.sql_start()
+    sqlite_db.sql_start()
+    # postgesql_db.sql_start()
 
 
 def sql_close():
-    postgesql_db.sql_close()
-
+    sqlite_db.sql_close()
+    # postgesql_db.sql_close()
 
 def sql_add_id_table_command(id):
-    postgesql_db.sql_add_id_table_command(id)
+    sqlite_db.sql_add_id_table_command(id)
+    # postgesql_db.sql_add_id_table_command(id)
 
 
 def sql_add_text_command(id, en, ru):
-    postgesql_db.sql_add_text_command(id, en, ru)
+    sqlite_db.sql_add_text_command(id, en, ru)
+    # postgesql_db.sql_add_text_command(id, en, ru)
 
 
 def sql_read_base(id):
-    return postgesql_db.sql_read_base(id)
+    return sqlite_db.sql_read_base(id)
+    # return postgesql_db.sql_read_base(id)
 
 
 def sql_update_date(id, en):
-    postgesql_db.sql_update_date(id, en)
+    sqlite_db.sql_update_date(id, en)
+    # postgesql_db.sql_update_date(id, en)
 
 
 def sql_del_command(id, en):
-    postgesql_db.sql_del_command(id, en)
+    sqlite_db.sql_del_command(id, en)
+    # postgesql_db.sql_del_command(id, en)
--- data_base/sqlite_db.py
+++ data_base/sqlite_db.py
@@ -2,27 +2,57 @@ import sqlite3 as sq
 
 
 def sql_start():
-    global base, cur
+    global base, cursor
     base = sq.connect('translate_memo.db')
-    cur = base.cursor()
+    cursor = base.cursor()
     if base:
         print('SQLite base connected OK')
 
+def sql_close():
+    cursor.close()
+    base.close()
 
 def sql_add_id_table_command(id):
-    base.execute(f'CREATE TABLE IF NOT EXISTS id{id}(en PRIMARY KEY, ru)')
+    base.execute(
+        f"""CREATE TABLE IF NOT EXISTS dict_en_user_id_%s
+            (
+                id_word int GENERATED ALWAYS AS IDENTITY,
+    	        en_word varchar(64) UNIQUE,
+    	        ru_word varchar(64) UNIQUE,
+                repeat_date date DEFAULT NOW()
+            );""", (id,)
+    )
     base.commit()
 
 
 def sql_add_text_command(id, en, ru):
-    cur.execute(f'INSERT INTO id{id} VALUES (?, ?)', (en, ru))
+    cursor.execute(
+        f"""INSERT INTO dict_en_user_id_%s(en_word, ru_word)
+                VALUES
+                (%s, %s);""", (id, en, ru)
+    )
     base.commit()
 
 
 def sql_read_base(id):
-    return cur.execute(f'SELECT * FROM id{id}').fetchall()
+    cursor.execute(
+        f"""SELECT * FROM dict_en_user_id_%s
+                ORDER BY repeat_date;""", (id,)
+    )
+    return cursor.fetchall()
+
+def sql_update_date(id, en):
+    cursor.execute(
+        f"""UPDATE dict_en_user_id_%s
+            SET repeat_date = NOW()
+            WHERE en_word = %s""", (id, en)
+    )
+    base.commit()
 
 
 def sql_del_command(id, en):
-    cur.execute(f'DELETE FROM id{id} WHERE en = \'{en}\'')
+    cursor.execute(
+        f"""DELETE FROM dict_en_user_id_%s
+                WHERE en_word = %s;""", (id, en)
+    )
     base.commit()
