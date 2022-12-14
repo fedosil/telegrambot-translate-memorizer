15c840e06dadc583c993cd08aa95320129b0c1a8
fedosil <fedosil@yandex.ru>
2022-12-14 10:00:12 +0300

    Update sql_db.py

--- data_base/sql_db.py
+++ data_base/sql_db.py
@@ -1,36 +1,29 @@
-# from data_base import sqlite_db as db
-from data_base import postgesql_db as db
+from data_base import sqlite_db as db
+# from data_base import postgesql_db as db
 
 
 def sql_start():
     db.sql_start()
-    # postgesql_db.sql_start()
 
 
 def sql_close():
     db.sql_close()
-    # postgesql_db.sql_close()
 
 def sql_add_id_table_command(id):
     db.sql_add_id_table_command(id)
-    # postgesql_db.sql_add_id_table_command(id)
 
 
 def sql_add_text_command(id, en, ru):
     db.sql_add_text_command(id, en, ru)
-    # postgesql_db.sql_add_text_command(id, en, ru)
 
 
 def sql_read_base(id):
     return db.sql_read_base(id)
-    # return postgesql_db.sql_read_base(id)
 
 
 def sql_update_date(id, en):
     db.sql_update_date(id, en)
-    # postgesql_db.sql_update_date(id, en)
 
 
 def sql_del_command(id, en):
     db.sql_del_command(id, en)
-    # postgesql_db.sql_del_command(id, en)
